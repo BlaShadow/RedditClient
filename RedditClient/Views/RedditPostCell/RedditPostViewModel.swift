39a76476cee89b68f4d0853652aee3cb3df407b6
blashadow <blackzerogamer@gmail.com>
2021-06-06 08:40:18 -0400

    create to computed properties to RedditPostViewModel

--- RedditClient/Views/RedditPostCell/RedditPostViewModel.swift
+++ RedditClient/Views/RedditPostCell/RedditPostViewModel.swift
@@ -13,4 +13,24 @@ class RedditPostViewModel {
   init(post: RedditPost) {
     self.post = post
   }
+  
+  var title: String? {
+    return self.post.title
+  }
+  
+  var subReddit: String? {
+    return "r/\(self.post.subreddit)"
+  }
+  
+  var author: String? {
+    return "u/\(self.post.author)"
+  }
+  
+  var thumbnail: String? {
+    return self.post.thumbnail
+  }
+  
+  var image: String? {
+    return self.post.url
+  }
 }
