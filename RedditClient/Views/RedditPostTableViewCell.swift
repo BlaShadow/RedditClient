1fd7908c4f295feff2bdde19007855df1261ebe3
blashadow <blackzerogamer@gmail.com>
2021-06-06 07:13:05 -0400

    dummy table view & cell

---
+++ RedditClient/Views/RedditPostTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  RedditPostTableViewCell.swift
+//  RedditClient
+//
+//  Created by Luis Romero on 6/6/21.
+//
+
+import UIKit
+
+class RedditPostTableViewCell: UITableViewCell {
+
+  override func awakeFromNib() {
+    super.awakeFromNib()
+  }
+
+  override func setSelected(_ selected: Bool, animated: Bool) {
+    super.setSelected(selected, animated: animated)
+  }
+}
