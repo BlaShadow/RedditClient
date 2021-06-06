32f9c6c60449e79b836f88ae6f7eb7d86574d9f9
blashadow <blackzerogamer@gmail.com>
2021-06-06 07:13:37 -0400

    Fetching reddit posts

--- RedditClient/ViewController.swift
+++ RedditClient/ViewController.swift
@@ -8,12 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
+  @IBOutlet weak var tableView: UITableView!
 
+  private var redditPosts: [TopReditItemContentServiceResponse] = []
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    
+    self.setupViews()
+  }
+  
+  private func setupViews() {
+    self.tableView.register(UINib(nibName: "RedditPostTableViewCell", bundle: nil), forCellReuseIdentifier: Constants.redditPostCellIdentifier)
+    self.tableView.tableFooterView = UIView()
   }
 
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
 
+    FacadeDataAccess.shared.fetchRedditPosts(completion: { [weak self] result in
+      guard let self = self else {
+        return
+      }
+
+      switch result {
+      case .success(let items):
+        self.redditPosts.append(contentsOf: items)
+      case .failure:
+        DialogHelper.displayAlert(title: "Error", message: "Error loading reddit posts", controller: self)
+      }
+    })
+  }
 }
 
