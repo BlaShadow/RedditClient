b15043876edb35a51f3caad791d9ca90abb73b7a
blashadow <blackzerogamer@gmail.com>
2021-06-06 09:45:17 -0400

    create a protocol for view models that contains a property reddit post

---
+++ RedditClient/DisplayableRedditPostProtocol.swift
@@ -0,0 +1,38 @@
+//
+//  DisplayableRedditPostProtocol.swift
+//  RedditClient
+//
+//  Created by Luis Romero on 6/6/21.
+//
+
+import UIKit
+
+protocol DisplayableRedditPostProtocol {
+  var post: RedditPost {get set}
+}
+
+extension DisplayableRedditPostProtocol {
+  var title: String? {
+    return self.post.title
+  }
+  
+  var subReddit: String? {
+    return "r/\(self.post.subreddit)"
+  }
+  
+  var author: String? {
+    return "u/\(self.post.author)"
+  }
+  
+  var thumbnail: String? {
+    return self.post.thumbnail
+  }
+  
+  var image: String? {
+    return self.post.url
+  }
+  
+  var comments: String {
+    return "\(self.post.numComments) comments"
+  }
+}
--- RedditClient/Views/RedditPostCell/RedditPostViewModel.swift
+++ RedditClient/Views/RedditPostCell/RedditPostViewModel.swift
@@ -8,29 +8,11 @@
 import UIKit
 
 class RedditPostViewModel {
-  let post: RedditPost
+  var post: RedditPost
   
   init(post: RedditPost) {
     self.post = post
   }
-  
-  var title: String? {
-    return self.post.title
-  }
-  
-  var subReddit: String? {
-    return "r/\(self.post.subreddit)"
-  }
-  
-  var author: String? {
-    return "u/\(self.post.author)"
-  }
-  
-  var thumbnail: String? {
-    return self.post.thumbnail
-  }
-  
-  var image: String? {
-    return self.post.url
-  }
 }
+
+extension RedditPostViewModel: DisplayableRedditPostProtocol {}
