967cbef17c64ebf2496a749af865498da2596885
blashadow <blackzerogamer@gmail.com>
2021-06-06 09:44:26 -0400

    comments number for post cell

--- RedditClient/Views/RedditPostCell/RedditPostTableViewCell.swift
+++ RedditClient/Views/RedditPostCell/RedditPostTableViewCell.swift
@@ -13,12 +13,14 @@ class RedditPostTableViewCell: UITableViewCell {
   @IBOutlet weak var authorLabel: UILabel!
   @IBOutlet weak var content: UILabel!
   @IBOutlet weak var postImage: UIImageView!
+  @IBOutlet weak var commentsLabel: UILabel!
   
   var viewModel: RedditPostViewModel? {
     didSet {
       self.authorLabel.text = self.viewModel?.author
       self.subRedditLabel.text = self.viewModel?.subReddit
       self.content.text = self.viewModel?.title
+      self.commentsLabel.text = self.viewModel?.comments
 
       // Load remove image
       FetchImagesHelper.fetchImage(url: self.viewModel?.thumbnail ?? "", into: self.postImage)
